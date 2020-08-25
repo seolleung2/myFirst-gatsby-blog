57864264f96bf2b289c8e56467c0b15b5746a3fa
seolleung2 <seolleung22@gmail.com>
2020-08-25 13:19:02 +0900

    add README.md and more

---
+++ README.md
@@ -0,0 +1,1 @@
+🧙🏻‍♀️My blog with Gatsby, Netlify and more..🥰
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `MyBlog`,
+  description: `Blog posted about TIL (Today I Learned)`,
+  author: `[seolleung2]`,
+  introduction: `개발을 스스로 해낼 수 있는 올바른 길을 습득하는 중입니다.`,
+  siteUrl: `https://dev-seolleung2.netlify.app/`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `seolleung2`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
