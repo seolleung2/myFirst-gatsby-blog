24d4d467e1c924900aceabb2a740e8887282bf1d
seolleung2 <seolleung22@gmail.com>
2020-09-03 12:34:41 +0900

    changed meta config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,11 @@
 module.exports = {
   title: `💻 seolleung2.dev`,
-  description: `Blog posted about TIL (Today I Learned)`,
+  description: `seollung2의 블로그입니다. 개발 학습과 관련한 글을 작성합니다.`,
   author: `[seolleung2]`,
   introduction: `개발을 스스로 해낼 수 있는 올바른 길을 습득하는 중입니다.`,
   siteUrl: `https://dev-seolleung2.netlify.app/`, // Your blog site url
+  sitemapPath: `https://dev-seolleung2.netlify.app/sitemap.xml`,
+  robotsPolicy: [{ userAgent: '*', allow: '/' }],
   social: {
     twitter: ``, // Your Twitter account
     github: `seolleung2`, // Your GitHub account
@@ -12,7 +14,17 @@ module.exports = {
     linkedin: ``, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [
+    `blog`,
+    `devlog`,
+    `web`,
+    `development`,
+    `frontend`,
+    `backend`,
+    `javascript`,
+    `books`,
+    `review`,
+  ],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
     utterances: 'seolleung2/myFirst-gatsby-blog', // Your repository for archive comment
